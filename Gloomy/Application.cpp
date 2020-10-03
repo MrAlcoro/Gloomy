251492f9b331baf2b65a0cb904b6e30d9b307ccf
MrAlcoro <alcoro13@gmail.com>
2020-10-03 15:48:42 +0200

    Using STD
    - Deleted p2Point.
    - Deleted p2DynArray.
    - Deleted p2List.

--- Gloomy/Application.cpp
+++ Gloomy/Application.cpp
@@ -26,12 +26,12 @@ Application::Application()
 
 Application::~Application()
 {
-	p2List_item<Module*>* item = list_modules.getLast();
+	list<Module*>::iterator item = list_modules.begin();
 
-	while(item != NULL)
+	while (item != list_modules.end())
 	{
-		delete item->data;
-		item = item->prev;
+		delete (item._Ptr->_Myval);
+		item++;
 	}
 }
 
@@ -40,22 +40,22 @@ bool Application::Init()
 	bool ret = true;
 
 	// Call Init() in all modules
-	p2List_item<Module*>* item = list_modules.getFirst();
+	list<Module*>::iterator item = list_modules.begin();
 
-	while(item != NULL && ret == true)
+	while (item != list_modules.end() && ret == true)
 	{
-		ret = item->data->Init();
-		item = item->next;
+		ret = item._Ptr->_Myval->Init();
+		item++;
 	}
 
 	// After all Init calls we call Start() in all modules
 	LOG("Application Start --------------");
-	item = list_modules.getFirst();
+	item = list_modules.begin();
 
-	while(item != NULL && ret == true)
+	while (item != list_modules.end() && ret == true)
 	{
-		ret = item->data->Start();
-		item = item->next;
+		ret = item._Ptr->_Myval->Start();
+		item++;
 	}
 	
 	ms_timer.Start();
@@ -80,28 +80,29 @@ update_status Application::Update()
 	update_status ret = UPDATE_CONTINUE;
 	PrepareUpdate();
 	
-	p2List_item<Module*>* item = list_modules.getFirst();
-	
-	while(item != NULL && ret == UPDATE_CONTINUE)
+
+	list<Module*>::iterator item = list_modules.begin();
+
+	while (item != list_modules.end() && ret == UPDATE_CONTINUE)
 	{
-		ret = item->data->PreUpdate(dt);
-		item = item->next;
+		ret = item._Ptr->_Myval->PreUpdate(dt);
+		item++;
 	}
 
-	item = list_modules.getFirst();
+	item = list_modules.begin();
 
-	while(item != NULL && ret == UPDATE_CONTINUE)
+	while (item != list_modules.end() && ret == UPDATE_CONTINUE)
 	{
-		ret = item->data->Update(dt);
-		item = item->next;
+		ret = item._Ptr->_Myval->Update(dt);
+		item++;
 	}
 
-	item = list_modules.getFirst();
+	item = list_modules.begin();
 
-	while(item != NULL && ret == UPDATE_CONTINUE)
+	while (item != list_modules.end() && ret == UPDATE_CONTINUE)
 	{
-		ret = item->data->PostUpdate(dt);
-		item = item->next;
+		ret = item._Ptr->_Myval->PostUpdate(dt);
+		item++;
 	}
 
 	FinishUpdate();
@@ -111,17 +112,17 @@ update_status Application::Update()
 bool Application::CleanUp()
 {
 	bool ret = true;
-	p2List_item<Module*>* item = list_modules.getLast();
+	list<Module*>::iterator item = list_modules.begin();
 
-	while(item != NULL && ret == true)
+	while (item != list_modules.end() && ret == true)
 	{
-		ret = item->data->CleanUp();
-		item = item->prev;
+		ret = item._Ptr->_Myval->CleanUp();
+		item++;
 	}
 	return ret;
 }
 
 void Application::AddModule(Module* mod)
 {
-	list_modules.add(mod);
+	list_modules.push_back(mod);
 }
